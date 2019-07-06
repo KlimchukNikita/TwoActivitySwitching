39590fa512335bcdcb79c49c97746d994a9cce7d
KlimchukNikita <KlimchukNikita@users.noreply.github.com>
2019-07-06 23:38:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/twoactivityswitching/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.twoactivityswitching;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    Button btnActTwo;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                // TODO Call second activity
+                Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+}
